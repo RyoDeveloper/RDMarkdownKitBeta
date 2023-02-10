8450c7601e1750c12cf85c45981df63751663435
RyoDeveloper <121738322+RyoDeveloper@users.noreply.github.com>
2023-02-10 15:21:31 +0900

    Add: コードブロックを折り返せるようにする

--- Sources/RDMarkdownKit/Colum/CodeBlockColumView.swift
+++ Sources/RDMarkdownKit/Colum/CodeBlockColumView.swift
@@ -1,6 +1,6 @@
 //
 //  CodeBlockColumView.swift
-//  
+//
 //
 //  https://github.com/RyoDeveloper/RDMarkdownKit
 //  Copyright © 2023 RyoDeveloper. All rights reserved.
@@ -9,9 +9,10 @@
 import SwiftUI
 
 struct CodeBlockColumView: View {
+    @AppStorage("isScroll") var isScroll = false
     @State var code: String
     @State var fileName: String?
-    
+
     init(_ text: String) {
         var hoge = text
         if hoge.prefix(3) == "```" {
@@ -21,24 +22,39 @@ struct CodeBlockColumView: View {
             hoge.removeLast(4)
         }
         self.code = hoge
-        
+
         fileName = "apple.swift"
     }
-    
+
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
             if let fileName {
                 Text(fileName)
                     .font(.footnote)
                     .padding([.top, .leading, .trailing], 4)
-                    .background(Color(.systemGray4))
+                    .background(Color.gray.opacity(0.1))
             }
-            Text(code)
-                .textSelection(.enabled)
-                .padding()
-                .font(.system(.body, design: .monospaced))
-                .frame(maxWidth: .infinity, alignment: .topLeading)
-                .background(Color(.systemGray4))
+            Group {
+                if isScroll {
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        Text(code)
+                    }
+                } else {
+                    Text(code)
+                }
+            }
+            .textSelection(.enabled)
+            .padding()
+            .font(.system(.body, design: .monospaced))
+            .frame(maxWidth: .infinity, alignment: .topLeading)
+            .background(Color.gray.opacity(0.1))
+            .contextMenu {
+                Button {
+                    isScroll.toggle()
+                } label: {
+                    Label(isScroll ? "折り返す" : "折り返さない", systemImage: isScroll ? "arrow.up.backward.and.arrow.down.forward" : "arrow.down.right.and.arrow.up.left")
+                }
+            }
         }
     }
 }
@@ -46,15 +62,15 @@ struct CodeBlockColumView: View {
 struct CodeBlockColumView_Previews: PreviewProvider {
     static var previews: some View {
         CodeBlockColumView("""
-```
-struct CodeBlockView: View {
-    @State var code = ""
+        ```
+        struct CodeBlockView: View {
+            @State var code = ""
 
-    var body: some View {
-        Text(code)
-    }
-}
-```
-""")
+            var body: some View {
+                Text(code)
+            }
+        }
+        ```
+        """)
     }
 }
